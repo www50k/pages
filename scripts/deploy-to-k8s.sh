801346a49c4085a4ad90ff3e0b5684730a082739
brixton <praveenjana@gmail.com>
2021-01-13 09:47:50 +0000

    Lab 9 - Scaling and PV

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -2,6 +2,10 @@
 set -ex
 
 kubectl apply -f deployment/pages-namespace.yaml
+
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
+
 kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
 kubectl apply -f deployment/pages-deployment.yaml
@@ -11,3 +15,4 @@ kubectl apply -f deployment/mysql-service.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
 kubectl apply -f deployment/flyway-configmap.yaml
 kubectl apply -f deployment/flyway-job.yaml
+
